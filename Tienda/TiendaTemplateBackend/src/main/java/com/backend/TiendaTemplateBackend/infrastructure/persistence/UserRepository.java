c8b11be45abed5f6c686a6c90cb3ee1c659e432e
Jesús <145559043+jesusmoncar@users.noreply.github.com>
2026-03-19 19:25:48 +0100

    Delete Tienda directory

--- Tienda/TiendaTemplateBackend/src/main/java/com/backend/TiendaTemplateBackend/infrastructure/persistence/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.TiendaTemplateBackend.infrastructure.persistence;
-
-import com.backend.TiendaTemplateBackend.domain.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
